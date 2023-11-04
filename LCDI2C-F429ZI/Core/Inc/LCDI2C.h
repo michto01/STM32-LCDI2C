da5e67af39aa86184d334103b61c9de82536d642
Tomas Michalek <tomas.michalek@spsehavirov.cz>
2023-11-04 16:12:36 +0100

    Update LCDI2C.h
    Move the header to C file as it is unnecessary here.

--- LCDI2C-F429ZI/Core/Inc/LCDI2C.h
+++ LCDI2C-F429ZI/Core/Inc/LCDI2C.h
@@ -8,14 +8,12 @@
 #ifndef _LCDI2C_H_
 #define _LCDI2C_H_
 
-#include "stm32f4xx_hal.h"
-
 void LCD_initID(uint8_t _ADDR);
-void LCD_Init();
-void LCD_sendCMD (char cmd);
-void LCD_sendData (char data);
+void LCD_Init(void);
+void LCD_Clear(void);
+void LCD_sendCMD(char cmd);
+void LCD_sendData(char data);
 void LCD_sendSTR(char *str);
-void LCD_Clear (void);
 void LCD_setCursor(uint8_t x, uint8_t y);
 
 #endif
